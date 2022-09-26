fe83747fbac216893a09ce00276992802f1d7a96
ntrocha <ntrocha@uninorte.edu.co>
2022-09-26 15:46:09 -0500

    fix historical problems

--- public/js/operations_db.js
+++ public/js/operations_db.js
@@ -36,7 +36,7 @@ function readRange(connection, callback){
     // var b = '1.664210299422E12';
     const a =1;
     const b =5;
-    connection.query(`SELECT * FROM prueba WHERE Time BETWEEN ${a} AND ${b}`, function (err, result){
+    connection.query(`SELECT * FROM prueba WHERE Lat BETWEEN ${a} AND ${b}`, function (err, result){
         if (err) throw err;
         console.log(result);
         callback(result);    
