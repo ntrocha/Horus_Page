a940ac073d5afb0e93b2f6b6392d5a3c5db1cfd2
ntrocha <ntrocha@uninorte.edu.co>
2022-09-26 09:26:19 -0500

    fix historical

--- public/js/historic.js
+++ public/js/historic.js
@@ -4,14 +4,14 @@ var dtnm2
 save = function (){
     var optdate1;
     var optdate2;
-    optdate = document.getElementById("since").value;
+    optdate1 = document.getElementById("since").value;
     console.log(optdate1);
-    dtnm1= parseFloat(Date.parse(optdate1));
+    dtnm1= optdate1;
     //dtnm1 = 2;
 
-    optdate = document.getElementById("until").value;
+    optdate2 = document.getElementById("until").value;
     console.log(optdate2);
-    dtnm2= parseFloat(Date.parse(optdate2));
+    dtnm2= optdate2;
     //dtnm2 = 5;
 },
 
