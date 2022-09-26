748609e8666eecaa18e75e02efc1a98c5dbcff75
ntrocha <ntrocha@uninorte.edu.co>
2022-09-26 13:18:58 -0500

    fix historical problems

--- public/js/historic.js
+++ public/js/historic.js
@@ -1,20 +1,22 @@
 
 var dtnm1
 var dtnm2
-save = function (){
-    var optdate1;
-    var optdate2;
-    optdate1 = document.getElementById("since").value;
-    console.log(optdate1);
-    dtnm1= Date.UTC(optdate1);
-    console.log(dtnm1);
-    //dtnm1 = 2;
+var boton = document.getElementById("btnH");
 
-    optdate2 = document.getElementById("until").value;
-    console.log(optdate2);
-    dtnm2= Date.UTC(optdate2);
-    console.log(dtnm2);
-    //dtnm2 = 5;
-},
+boton.onclick = function (){
+        var optdate1;
+        var optdate2;
+        optdate1 = document.getElementById("since").value;
+        console.log(optdate1);
+        dtnm1= Date.UTC(optdate1);
+        console.log(dtnm1);
+        //dtnm1 = 2;
+
+        optdate2 = document.getElementById("until").value;
+        console.log(optdate2);
+        dtnm2= Date.UTC(optdate2);
+        console.log(dtnm2);
+        //dtnm2 = 5;
+    },
 
 module.exports = {dtnm1, dtnm2};
