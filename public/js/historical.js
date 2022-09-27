6a8a034a17e1078dace72148201c32dcda7c5575
ntrocha <ntrocha@uninorte.edu.co>
2022-09-26 20:30:37 -0500

    fix datetype to historic2

--- public/js/historical.js
+++ public/js/historical.js
@@ -1,7 +1,7 @@
 
 optdate1 = (document.getElementById("since").value).toString();
 if (optdate1){
-
+function historical () {
     var marker = L.marker([51.5, -0.09]);
     var marker2 = L.marker([51.5, -0.09]);
     var map = L.map('map2').setView([51.505, -0.09], 13);
@@ -53,4 +53,5 @@ if (optdate1){
                 };
             }
             aplicacion.Leer();
+    }
 }
